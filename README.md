8268dd2149fd29c9cb2f14f8bad35c9319316225
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2014-09-13 17:32:32 -0400

    Only schedule holidays in service date range.
    This silences feed validation warnings about dates without service.

--- README.md
+++ README.md
@@ -18,12 +18,14 @@ To use:
     
 2. Write the new trips.txt and stop\_times.txt by running make\_trips\_stops.py:
         `python make_trips_stops.py`
-        
-3. If the scheduled holidays need updating, modify use\_holidays.txt and run make\_calendar\_dates.py:
+
+3. Modify calendar.txt in the gtfs\_files directory to set the schedule's effective date.
+
+4. Run make\_calendar\_dates.py, first modifying use\_holidays.txt if the holidays in range have changed.
+   Be sure to update calendar.txt first, as holidays will be scheduled within the effective date range.
+
         `python make_calendar_dates.py`
         
-4. If the fare prices need updating, modify the fare\_attributes.txt file directly in the gtfs\_files directory.
-
-5. Modify calendar.txt in the gtfs\_files directory to set the schedule's effective date.
+5. If the fare prices need updating, modify the fare\_attributes.txt file directly in the gtfs\_files directory.
 
 6. Zip the contents of the gtfs\_files directory, and validate the results with [Google's GTFS validator](https://code.google.com/p/googletransitdatafeed/wiki/FeedValidator).
--- make_calendar_dates.py
+++ make_calendar_dates.py
@@ -2,8 +2,21 @@
 import csv
 
 inf = open('use_holidays.txt', 'r')
+calf = open('gtfs_files/calendar.txt', 'r')
 outf = open('gtfs_files/calendar_dates.txt', 'w')
 header = ['service_id','date','exception_type']
+
+# first read calendar file to determine valid date range
+rdr = csv.reader(calf)
+# skip header row
+rdr.next()
+svc = rdr.next()
+# start and end dates are last two columns in file
+start_date = int(svc[len(svc)-2])
+end_date = int(svc[len(svc)-1])
+calf.close()
+
+print('Scheduling holiday services for dates between %s and %s.' % (start_date, end_date))
 
 writer = csv.writer(outf, quoting=csv.QUOTE_NONE)
 reader = csv.reader(inf)
@@ -15,10 +28,14 @@ for ln in reader:
   elif ln[0].startswith('-'):
     next
   else:
-    # remove weekday service
-    writer.writerow([3,ln[0],2])
-    # add sunday service
-    writer.writerow([1,ln[0],1])
+    holiday = int(ln[0])
+    if holiday >= start_date and holiday <= end_date:
+      # remove weekday service
+      writer.writerow([3,ln[0],2])
+      # add sunday service
+      writer.writerow([1,ln[0],1])
+    else:
+      print('Skipping holiday entry out of range, on %s.' % ln[0])
 
 outf.close()
 inf.close()
