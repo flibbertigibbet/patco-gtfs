4f6422c8880d153abcf457459f0f26b2391359be
thegreengrass <jrussell@redtettemer.com>
2015-09-03 10:45:02 -0400

    Updating for the September 4th and 7th special schedules as well as the new schedule effective September 8th

--- make_trips_stops.py
+++ make_trips_stops.py
@@ -9,7 +9,7 @@ estops.reverse() # Philly to Lindenwold
 route_id = 12
 
 # map the csv file names to the days of the week for the service IDs in calendar.txt
-service_ids = { 'saturday_sunday': 1, 'monday_friday': 2, 'july2': 3, 'july3': 4, 'july4': 5 }
+service_ids = { 'monday_wednesday': 1, 'thursday': 2, 'friday': 3, 'saturday_sunday': 4, 'sept4': 5, 'sept7': 6 }
 
 # TODO: what's the headsign for the westbound express and train from woodcrest?
 DIR = {'eastbound':[1, "LINDENWOLD LOCAL", estops],
